dbdeb82fbd893324f03264dd02a0f3e372e4e614
Rolf Jagerman <rjagerman@gmail.com>
2015-10-21 17:35:12 +0200

    Add more documentation to client interface and privatized variables

--- src/main/scala/glint/Client.scala
+++ src/main/scala/glint/Client.scala
@@ -10,8 +10,20 @@ import glint.models.BigModel
 import scala.reflect.ClassTag
 
 /**
- * A client interface that facilitates easy communication with the master
+ * A client interface that facilitates easy communication with the master and provides easy-to-use functions to spawn
+ * large models on the parameter servers.
  *
+ * A typical usage scenario (pull/push a distributed dense array with 10000 values initialized to 0.0):
+ *
+ * {{{
+ *   val client = new Client()
+ *   val bigModel = client.dense[Double](10000, 0.0)
+ *
+ *   bigModel.pull(Array(1, 2, 5000, 8000)).onSuccess { case values => println(values.mkString(", ")) }
+ *   bigModel.push(Array(1, 2, 300, 40), Array(0.1, 0.2, 300.2, 0.001))
+ * }}}
+ *
+ * @constructor Create a new client with optional configuration (see default.conf for an example)
  * @param config The configuration
  */
 class Client(config: Config = ConfigFactory.parseFile(new File(getClass.getClassLoader.getResource("default.conf").getFile))) extends StrictLogging {
@@ -40,13 +52,19 @@ class Client(config: Config = ConfigFactory.parseFile(new File(getClass.getClass
   private val masterSystem = config.getString("glint.master.system")
 
   @transient
-  lazy val system = ActorSystem(config.getString("glint.client.system"), akkaConfig)
+  private[glint] lazy val system = ActorSystem(config.getString("glint.client.system"), akkaConfig)
 
   @transient
-  lazy val master = system.actorSelection(s"akka.tcp://${masterSystem}@${masterHost}:${masterPort}/user/${masterName}")
+  private[glint] lazy val master = system.actorSelection(s"akka.tcp://${masterSystem}@${masterHost}:${masterPort}/user/${masterName}")
 
   /**
    * Constructs a distributed dense array (indexed by Long) over the parameter servers
+   * The returned model is serializable and safe to be used across machines (e.g. in Spark workers)
+   *
+   * Typical usage:
+   * {{{
+   *   val model = client.dense[Double](10000, 0.0)
+   * }}}
    *
    * @param size The total size
    * @param default The default value to populate the array with
