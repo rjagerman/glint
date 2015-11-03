45cb91105674a8505cbfe359e775fed9fd85a1e2
Rolf Jagerman <rjagerman@gmail.com>
2015-11-03 11:47:06 +0100

    Clean up server start up code

--- src/main/scala/glint/Server.scala
+++ src/main/scala/glint/Server.scala
@@ -1,21 +1,26 @@
 package glint
 
-import akka.actor.{Actor, Props, ActorSystem}
+import java.util.concurrent.TimeUnit
+
+import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
-import com.typesafe.config.{Config, ConfigFactory}
-import com.typesafe.scalalogging.StrictLogging
-import glint.messages.master.Register
-import scala.concurrent.ExecutionContext
+import com.typesafe.config.{ConfigValueFactory, Config, ConfigFactory}
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import glint.Master._
+import glint.messages.master.RegisterServer
+import scala.concurrent.{Future, ExecutionContext}
 import scala.concurrent.duration._
 
 /**
  * A parameter server
  */
-class Server extends Actor with StrictLogging {
+class Server extends Actor with ActorLogging {
+
   override def receive: Receive = {
+
     case x =>
-      logger.warn(s"Received unknown message of type ${x.getClass}")
+      log.warning(s"Received unknown message of type ${x.getClass}")
   }
 }
 
@@ -27,35 +32,20 @@ object Server extends StrictLogging {
   /**
    * Starts a parameter server ready to receive commands
    *
+   * @param config The configuration
    * @param host The host name
    * @param port The port
    */
-  def run(config: Config, host: String, port: Int): Unit = {
+  def run(config: Config, host: String, port: Int): Future[(ActorSystem, ActorRef)] = {
 
-    logger.debug("Creating akka remote configuration")
-    val akkaConfig = ConfigFactory.parseString(
-      s"""
-      akka {
-        event-handlers = ["akka.event.slf4j.Slf4jEventHandler"]
-        loglevel = "INFO"
-        actor {
-          provider = "akka.remote.RemoteActorRefProvider"
-        }
-        remote {
-          enable-transports = ["akka.remote.netty.tcp"]
-          netty.tcp {
-            hostname = ${host}
-            port = ${port}
-          }
-        }
-      }
-      """.stripMargin)
+    var modifiedConfig = config.withValue("glint.server.akka.remote.netty.tcp.hostname", ConfigValueFactory.fromAnyRef(host))
+    modifiedConfig = modifiedConfig.withValue("glint.server.akka.remote.netty.tcp.port", ConfigValueFactory.fromAnyRef(port))
 
-    logger.debug(s"Starting actor system ${config.getString("glint.worker.system")}@${host}:${port}")
-    val system = ActorSystem(config.getString("glint.worker.system"), akkaConfig)
+    logger.debug(s"Starting actor system ${config.getString("glint.server.system")}@${host}:${port}")
+    val system = ActorSystem(config.getString("glint.server.system"), modifiedConfig.getConfig("glint.server"))
 
     logger.debug("Starting server actor")
-    val ps = system.actorOf(Props[Server])
+    val server = system.actorOf(Props[Server], config.getString("glint.server.name"))
 
     logger.debug("Reading master information from config")
     val masterHost = config.getString("glint.master.host")
@@ -64,24 +54,38 @@ object Server extends StrictLogging {
     val masterSystem = config.getString("glint.master.system")
 
     logger.info(s"Registering with master ${masterSystem}@${masterHost}:${masterPort}/user/${masterName}")
-    val master = system.actorSelection(s"akka.tcp://${masterSystem}@${masterHost}:${masterPort}/user/${masterName}")
-
     implicit val ec = ExecutionContext.Implicits.global
-    implicit val timeout = Timeout(10 seconds)
-    val registration = ask(master, Register())
+    implicit val timeout = Timeout(config.getDuration("glint.server.registration-timeout", TimeUnit.MILLISECONDS) milliseconds)
+    val master = system.actorSelection(s"akka.tcp://${masterSystem}@${masterHost}:${masterPort}/user/${masterName}")
+    val registration = master ? RegisterServer(server)
+
+    registration.map {
+      case a =>
+        logger.info("Server successfully registered with master")
+        (system, server)
+    }
+
+    /*registration.transform {
+      case a: ActorRef =>
+        logger.info(master.path.toSerializationFormatWithAddress(address))
+        logger.info("Master successfully started")
+        (system, server)
+    }
+
     registration.onSuccess {
       case true =>
         logger.info(s"Succesfully registered with master")
       case _ =>
         logger.error(s"Unknown response from master")
-        system.terminate()
+        system.shutdown()
     }
+
     registration.onFailure {
       case e: Exception =>
         logger.error(s"Unable to register with master")
         logger.error(e.getMessage)
-        system.terminate()
-    }
+        system.shutdown()
+    }*/
 
   }
 }
