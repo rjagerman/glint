b64c2c109f8052eacc9b18a2a925df69cb94bd1c
Rolf Jagerman <rjagerman@gmail.com>
2015-11-19 17:47:14 +0100

    Startup scripts now use env.sh configuration

--- sbin/start-master.sh
+++ sbin/start-master.sh
@@ -6,5 +6,5 @@ GLINT_SBIN_PATH=`cd $(dirname ${BASH_SOURCE[0]}); pwd`
 source $GLINT_SBIN_PATH/configuration.sh
 
 # Start master
-nohup java -jar $GLINT_PATH/bin/Glint-assembly-0.1-SNAPSHOT.jar master -c $GLINT_PATH/conf/default.conf > $GLINT_PATH/logs/master-out.log 2> $GLINT_PATH/logs/master-err.log &
+nohup java $GLINT_MASTER_OPTS -jar $GLINT_JAR_PATH master -c $GLINT_PATH/conf/default.conf > $GLINT_PATH/logs/master-out.log 2> $GLINT_PATH/logs/master-err.log &
 echo $! >> $GLINT_PATH/pids/master.pid
